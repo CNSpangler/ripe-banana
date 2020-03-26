0ad8fca9d39d8ada4907a94df5a93f8533f00739
Chelsea Spangler <chelseanspangler@gmail.com>
2020-03-26 14:14:40 -0700

    add seed file

--- db/seed.js
+++ db/seed.js
@@ -1,21 +1,23 @@
-const Book = require('../lib/models/Book');
-const Author = require('../lib/models/Author');
+const Studio = require('../lib/models/Studio');
+// const Actor = require('../lib/models/Actor');
+// const Reviewer = require('../lib/models/Reviewer');
 const chance = require('chance').Chance();
 
 // specifying the number of tweets to create with our seed function
-module.exports = async({ authorsToCreate = 10, booksToCreate = 100 } = {}) => {
-  const authors = await Author.create([...Array(authorsToCreate)].map(() => ({
-    name: chance.name()
+module.exports = async({ studiosToCreate = 10 } = {}) => {
+  const studios = await Studio.create([...Array(studiosToCreate)].map(() => ({
+    name: chance.name(),
+    address: {
+      city: chance.city(),
+      state: chance.state(),
+      country: chance.country()
+    }
   })));
-  // creating tweets
-  // creating an array of tweetsToCreate length
-  // map through the array
-  // -> for each item in the array we create an object with { handle, text }
-  // for each tweet in the mapped array we create a tweet in our mongodb
-  const genres = ['Science Fiction', 'Fantasy', 'Non-Fiction', 'YA', 'Horror', 'Romance'];
-  await Book.create([...Array(booksToCreate)].map(() => ({
-    authorId: chance.pickone(authors)._id,
-    title: chance.sentence(),
-    genre: chance.pickone(genres),
-  })));
+
+  // const genres = ['Science Fiction', 'Fantasy', 'Non-Fiction', 'YA', 'Horror', 'Romance'];
+  // await Book.create([...Array(booksToCreate)].map(() => ({
+  //   authorId: chance.pickone(authors)._id,
+  //   title: chance.sentence(),
+  //   genre: chance.pickone(genres),
+  // })));
 };
